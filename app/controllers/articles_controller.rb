d583538909c314704666f67f08b69cd8929bf28c
c-santana-sp <carlos.santana@bandtec.com.br>
2022-04-12 00:10:40 -0300

    refactor: adding before_action methods to article_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@articles = Article.all
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(get_article_params)
 		if @article.save
 			flash[:notice] = "Article was created successfully."
 			redirect_to article_path(@article.id)
@@ -27,8 +26,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(get_article_params)
 			flash[:notice] = "Article was updated successfully."
 			redirect_to article_path(@article.id)
 		else
@@ -40,5 +38,14 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
+	end
+
+	private
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def get_article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
